a2ceaa80d4f4cb25e7c9080d71f6f50968c4ba45
jappe999 <jappe999@users.noreply.github.com>
2017-02-01 21:02:12 +0100

    Update install_mac.sh

--- install_mac.sh
+++ install_mac.sh
@@ -1,1 +1,5 @@
-pip3 install pyobjc-core pyobjc pyautogui pyobjc-framework-Quartz gi
+pip3 install pyobjc-core
+pip3 install pyobjc 
+pip3 install pyautogui 
+pip3 install pyobjc-framework-Quartz 
+pip3 install gi
